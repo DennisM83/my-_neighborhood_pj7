d368a84e0719785e978d56f2e67e452903254934
DennisM83 <dennisfinley@gmail.com>
2018-10-16 14:34:43 -0400

    added Map.js and rendered map / added Style.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Map from './Map.js'
 import './App.css';
+import './Style.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <Map/>
       </div>
     );
   }
---
+++ src/Map.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+        < GoogleMap defaultZoom = {
+            14
+        }
+        defaultCenter = {
+            {
+                lat: 28.688239,
+                lng: -81.399993
+            }
+        } >
+        {
+            props.isMarkerShown && < Marker position = {
+                {
+                    lat: 28.688239,
+                    lng: -81.399993
+                }
+            }
+            />}
+        </GoogleMap>
+))
+
+class Map extends Component {
+    render() {
+        return(
+            <MyMapComponent
+                isMarkerShown
+                googleMapURL = "https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAbs0suPVRKY8V7nXTVVjoWeVZYJjxqQNU"
+                loadingElement = { <div style = {{ height: `100%` }}/>}
+                containerElement = { <div style = {{ height: `400px` }}/>}
+                mapElement = {<div style = {{ height: `100vh` }}/>} 
+            />
+        )
+    }
+}
+
+export default Map;
