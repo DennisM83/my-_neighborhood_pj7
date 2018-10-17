6c880fb66493c76132300e4acf4ee166a8bad587
DennisM83 <dennisfinley@gmail.com>
2018-10-17 12:17:15 -0400

    setup foursquare API

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 import Map from './Map.js'
 import './App.css';
 import './Style.css'
+import SquareAPI from './API/';
 
 class App extends Component {
+
+  componentDidMount() {
+    SquareAPI.search({
+      ll: '28.688239,-81.399993',
+      query: "food",
+      limit: 10
+    }).then(result => console.log(result))
+  }
+
   render() {
     return (
       <div className="App">
--- src/Map.js
+++ src/Map.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
+
+/*From Tom Chen @ https://tomchentw.github.io/react-google-maps/ */
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-        < GoogleMap defaultZoom = {
-            14
-        }
-        defaultCenter = {
-            {
-                lat: 28.688239,
-                lng: -81.399993
-            }
-        } >
-        {
-            props.isMarkerShown && < Marker position = {
-                {
-                    lat: 28.688239,
-                    lng: -81.399993
-                }
-            }
+        <GoogleMap 
+            defaultZoom = { 14 }
+            defaultCenter = {
+            {   lat: 28.688239, lng: -81.399993 }}
+        >
+        {props.isMarkerShown && < Marker position = {
+                {   lat: 28.688239, lng: -81.399993 }}
             />}
         </GoogleMap>
 ))
+
+window.gm_authFailure = () => {
+    alert('Oops! looks like something went wrong!')
+};
 
 class Map extends Component {
     render() {
