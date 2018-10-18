40f7bc2bf12e95fd6e44fcc946378663b7592e6b
DennisM83 <dennisfinley@gmail.com>
2018-10-18 09:04:30 -0400

    update to the CSS / added borders around the names in sidebar

--- src/Map.js
+++ src/Map.js
@@ -11,20 +11,25 @@ const MyMapComponent = withScriptjs(withGoogleMap((props) =>
         >
         { /*written by Forrest Walker - https://www.youtube.com/watch?v=cJ3sAG2Ybq4&index=4&list=PL4rQq4MQP1crXuPtruu_eijgOUUXhcUCP */}
         {props.markers && props.markers.filter(marker => marker.isVisible).map((marker, index) => {
+            /* compares the ids between the venue and the current marker */
             let venueInfo =  props.venues.find(venue => venue.id === marker.id)
+            /* renders markers dynamically */
            return <Marker key={index} position = {{ lat: marker.lat, lng: marker.lng }} defaultAnimation={window.google.maps.Animation.DROP} onClick={() => props.markerClick(marker)}>
                 {marker.isOpen && (
+                    /* the info window to display the name of the establishment and the address */
                 <InfoWindow>
                     <div>
                         <p>{venueInfo.name}</p>
                         <p>{venueInfo.location.address}</p>
-                    </div></InfoWindow>)}
+                    </div>
+                </InfoWindow>)}
             </Marker>
         })
         }
         </GoogleMap>
 ))
 
+/* error handler for the map in case of failure*/
 window.gm_authFailure = () => {
     alert('Oops! looks like something went wrong loading the map')
 };
@@ -33,12 +38,16 @@ class Map extends Component {
     render() {
         return(
             <MyMapComponent
+            /* spread operator for the array props */
                 {...this.props}
                 isMarkerShown
+            /* the url to render GOOGLE MAPS */
                 googleMapURL = "https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAbs0suPVRKY8V7nXTVVjoWeVZYJjxqQNU"
                 loadingElement = { <div style = {{ height: `100%` }}/>}
                 containerElement = { <div style = {{ height: `400px` }}/>}
-                mapElement = {<div style = {{ height: `100vh` }}/>} 
+            /* sets the relative total height of the window */
+                mapElement = {<div style = {{ height: `100vh` }}/>}
+                role="application" 
             />
         )
     }
--- src/SideMenu.js
+++ src/SideMenu.js
@@ -30,10 +30,12 @@ class SideMenu extends Component {
 
         return (
             <div className="sideMenu">
+            {/* checks the toggle open and close */}
                 {this.props.openMenu &&
                     <nav className="navbar navbar-dark bg-dark" id="navMenu">
                         <span className="navbar-span" id="navbar-span">
                         Local Eats
+                        {/* the filter for the list items */}
                         <form className="form-inline align-items-center col-auto">
                             <input className="form-control mr-sm-1" type="search" placeholder="filter" value={this.state.query} onChange={(event) => {this.reviseQuery(event.target.value)}} aria-label="filter"/>
                             <ul className="list-unstyled">
